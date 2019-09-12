370bc7f4c45ea1a7850f233c646589df5aba179c
Pedro Lemos <pedroemlemos@gmail.com>
2019-09-11 22:29:28 -0300

    Including dragon update method

--- src/app/dragons/service/dragons.service.ts
+++ src/app/dragons/service/dragons.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { DragonRequest } from '../integration/dragon.request';
 import { DragonListItemModel } from '../model/dragon-list-item.model';
 import { DragonModel } from '../model/dragon.model';
 import { DragonsRestService } from './dragons-rest.service';
@@ -18,6 +19,13 @@ export class DragonsService {
 
   public getDragon(id: string): Observable<DragonModel> {
     return this.restService.getDragon(id).pipe(
+      map(dragon => new DragonModel(dragon)),
+      catchError(() => of(undefined)),
+    );
+  }
+
+  public updateDragon(id: string, dragonRequest: DragonRequest): Observable<DragonModel> {
+    return this.restService.putDragon(id, dragonRequest).pipe(
       map(dragon => new DragonModel(dragon)),
       catchError(() => of(undefined)),
     );
